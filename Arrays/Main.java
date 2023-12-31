36377c0cfa448e2af78aeee8a938efbfb3fd6aa9
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2023-12-31 19:33:02 +0500

    Arrays Practice Question

--- Arrays/Main.java
+++ Arrays/Main.java
@@ -3,6 +3,22 @@ package Arrays;
 // Arrays Size need to define First.
 
 public class Main {
+
+    // Take Array as Input and find the Index of element given.
+    public static int finding_index(int[] array, int element) {
+        int counter = 0;
+        int result = 0;
+
+        while (counter < array.length) {
+            if (array[counter] == element) {
+                result = counter;
+                break;
+            }
+            counter += 1;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int Array[] = new int[3]; // Empty Array 0.
         int[] array = { 19, 20, 50 };
@@ -13,5 +29,8 @@ public class Main {
         for (int j = 0; j < Array.length; j++) {
             System.out.println(Array[j]);
         }
+
+        // Function Calling
+        System.out.println(finding_index(array, 20));
     }
 }
