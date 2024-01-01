4ce06487446e1e4f355e987bf743bfffff4aa557
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-01-01 23:23:56 +0500

    Enhanced For Loop

--- Loops/for_loop.java
+++ Loops/for_loop.java
@@ -1,9 +1,23 @@
 package Loops;
 
 public class for_loop {
+    public static void for_advance() {
+        int[] array = { 10, 12, 13, 14 };
+        // String str = "Hello";
+        for (int arr : array) {
+            System.out.print(arr + " ");
+        }
+        // for (char ch : str) {
+        // System.out.print(str + " ");
+        // }
+
+    }
+
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
+        for_advance();
     }
+
 }
